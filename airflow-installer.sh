1ef99d27737b9ecf4b1e007b2f51a0c5adb4c582
Ubuntu <ubuntu@ip-172-31-29-128.eu-west-2.compute.internal>
2020-03-01 12:28:09 +0000

    Airflow installer

---
+++ airflow-installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+// Set variables
+export HOME=/home/ubuntu
+export AIRFLOW_HOME=$HOME/venv/airflow
+
+// Install (or update) python3, pip, and virtualenv
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install virtualenv -y
+
+// Navigate to home directory
+cd $HOME
+
+// Create virtual environment inside home directory
+virtualenv -p `which python3` venv
+
+// Create airflow directory
+mkdir $AIRFLOW_HOME
+
+// Activate virtual environment
+source $HOME/venv/bin/activate
+
+// Install airflow
+pip install apache-airflow
+
+exit
+
